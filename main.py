ce74c07342b70b8a334c3fe2ec61257755935f3e
Amy Deng <dengk@berkeley.edu>
2023-03-09 19:30:07 -0800

    debug

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-from rsi.dataset.example_datasets.Bbh import Bbh
+from rsi.dataset.example_datasets.Aqua import Aqua
+inferences = ['(A)', '(B)', '(C)', '(D)']
+aqua = Aqua()
+out = aqua.filter_generated_paths(aqua.train[0], inferences)
+print(out)
