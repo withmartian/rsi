03b0f3ebcb01d9e3566ef23f6cb0f92aa72485dc
Amy Deng <dengk@berkeley.edu>
2023-03-23 18:22:37 -0700

    Mmlu debug

--- rsi/dataset/example_datasets/Mmlu.py
+++ rsi/dataset/example_datasets/Mmlu.py
@@ -7,10 +7,14 @@ from Dataset import Dataset
 class Mmlu(Dataset):
   name = "mmlu"
   classes = ['abstract_algebra', 'anatomy', 'astronomy', 'business_ethics', 'clinical_knowledge', 'college_biology', 'college_chemistry', 'college_computer_science', 'college_mathematics', 'college_medicine', 'college_physics', 'computer_security', 'conceptual_physics', 'econometrics', 'electrical_engineering', 'elementary_mathematics', 'formal_logic', 'global_facts', 'high_school_biology', 'high_school_chemistry', 'high_school_computer_science', 'high_school_european_history', 'high_school_geography', 'high_school_government_and_politics', 'high_school_macroeconomics', 'high_school_mathematics', 'high_school_microeconomics', 'high_school_physics', 'high_school_psychology', 'high_school_statistics', 'high_school_us_history', 'high_school_world_history', 'human_aging', 'human_sexuality', 'international_law', 'jurisprudence', 'logical_fallacies', 'machine_learning', 'management', 'marketing', 'medical_genetics', 'miscellaneous', 'moral_disputes', 'moral_scenarios', 'nutrition', 'philosophy', 'prehistory', 'professional_accounting', 'professional_law', 'professional_medicine', 'professional_psychology', 'public_relations', 'security_studies', 'sociology', 'us_foreign_policy', 'virology', 'world_religions']
+  # fp = os.path.join(os.path.dirname(__file__), 'cot_prompts/mmlu_cot.json')
+  if not os.path.exists("cot_prompts/mmlu_cot.json"):
+    print("*"*10)
+    print(os.path)
   with open("cot_prompts/mmlu_cot.json", "r") as f:
     cot_prompts = json.load(f)
-  with open("mmlu_random_50*57_samples.json", "r") as f:
-    shortened_dataset = json.load(f)
+  # with open("mmlu_random_50*57_samples.json", "r") as f:
+  #   shortened_dataset = json.load(f)
 
   def __init__(self, random_seed = 0):
     """
