c1309c86a9b0ea2962da7d8d2dd9dcf4f3e89add
Amy Deng <dengk@berkeley.edu>
2023-03-28 22:51:15 -0700

    refactor evaluate

--- rsi/Files/evaluate.py
+++ rsi/Files/evaluate.py
@@ -20,6 +20,16 @@ def get_checkpoint_states(checkpoint_dir, resume_from_checkpoint, iteration):
         states = json.load(f)
   return states
 
+def save_evaluation(performance, data_accuracy, performance_fp, dataset_slug, states, checkpoint_dir):
+    performance.append(data_accuracy)
+    # save performance
+    with open(performance_fp, "w") as f:
+        json.dump(performance, f)
+    # update states
+    states["completed_datasets"].append(dataset_slug)
+    with open(f'{checkpoint_dir}/states.json', "w") as f:
+        json.dump(states, f)
+
 def evaluate(iteration, eval_datasets: List[Tuple[Dataset, str]], model, tokenizer, resume_from_checkpoint=False, checkpoint_dir="eval_checkpoints", batch_size=16, save_every=50):
     """
     eval_datasets: a list of tuples containing dataset object (ex. a Mmlu instance) and eval method ("cot" or "direct")
@@ -27,30 +37,43 @@ def evaluate(iteration, eval_datasets: List[Tuple[Dataset, str]], model, tokeniz
     # checkpoint
     states = get_checkpoint_states(checkpoint_dir, resume_from_checkpoint, iteration)
     performance = []
-    performance_fp = f'iteration-{iteration}-performance.json'
+    performance_fp = f'{checkpoint_dir}/iteration-{iteration}-performance.json'
     if resume_from_checkpoint and os.path.exists(performance_fp):
         with open(performance_fp, "r") as f:
             performance = json.load(f)
 
     for dataset, method in eval_datasets:
-        if dataset.name not in states["completed_datasets"]:
-            data_accuracy = {}
-            if hasattr(dataset, "classes"):
-                for c in dataset.classes:
-                    accuracy = dataset.eval(model, tokenizer, dataset.train[c], batch_size, class_name=c, method=method, save_every=save_every, resume_from_checkpoint=resume_from_checkpoint, checkpoint_dir=checkpoint_dir)
-                    data_accuracy[f'{dataset.name}-{c}'] = accuracy
-            else:
+        data_accuracy = {}
+        if hasattr(dataset, "classes"):
+           for c in dataset.classes:
+              if f'{dataset.name}-{c}' not in states["completed_datasets"]:
+                accuracy = dataset.eval(model, tokenizer, dataset.train[c], batch_size, class_name=c, method=method, save_every=save_every, resume_from_checkpoint=resume_from_checkpoint, checkpoint_dir=checkpoint_dir)
+                data_accuracy[f'{dataset.name}-{c}'] = accuracy
+                save_evaluation(performance, data_accuracy, performance_fp, f'{dataset.name}-{c}', states, checkpoint_dir)
+        else: # dataset don't have classes
+           if dataset.name not in states["completed_datasets"]:
                 accuracy = dataset.eval(model, tokenizer, dataset.train, batch_size, class_name=None, method=method, save_every=save_every, resume_from_checkpoint=resume_from_checkpoint, checkpoint_dir=checkpoint_dir)
                 data_accuracy[dataset.name] = accuracy
-            print(f'{dataset.name} accuracy: {data_accuracy}')
-            performance.append(data_accuracy)
-            # save performance
-            with open(performance_fp, "w") as f:
-                json.dump(performance, f)
-            # update states
-            states["completed_datasets"].append(dataset.name)
-            with open(f'{checkpoint_dir}/states.json', "w") as f:
-                json.dump(states, f)
+                save_evaluation(performance, data_accuracy, performance_fp, dataset.name, states, checkpoint_dir)
+        
+        # if dataset.name not in states["completed_datasets"]:
+        #     data_accuracy = {}
+        #     if hasattr(dataset, "classes"):
+        #         for c in dataset.classes:
+        #             accuracy = dataset.eval(model, tokenizer, dataset.train[c], batch_size, class_name=c, method=method, save_every=save_every, resume_from_checkpoint=resume_from_checkpoint, checkpoint_dir=checkpoint_dir)
+        #             data_accuracy[f'{dataset.name}-{c}'] = accuracy
+        #     else:
+        #         accuracy = dataset.eval(model, tokenizer, dataset.train, batch_size, class_name=None, method=method, save_every=save_every, resume_from_checkpoint=resume_from_checkpoint, checkpoint_dir=checkpoint_dir)
+        #         data_accuracy[dataset.name] = accuracy
+        #     print(f'{dataset.name} accuracy: {data_accuracy}')
+        #     performance.append(data_accuracy)
+        #     # save performance
+        #     with open(performance_fp, "w") as f:
+        #         json.dump(performance, f)
+        #     # update states
+        #     states["completed_datasets"].append(dataset.name)
+        #     with open(f'{checkpoint_dir}/states.json', "w") as f:
+        #         json.dump(states, f)
     return performance
 
 def main():
